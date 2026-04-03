b926e401d811944143f58ca2f2a516f4f9d681a2
Pedro Lucas Adorno <Pedrolucasadorno254@gmail.com>
2026-04-03 19:34:40 -0300

    feat: start of Category create

--- src/domain/product/entity/category.js
+++ src/domain/product/entity/category.js
@@ -1,12 +1,25 @@
 class Category {
     constructor(id, title, description, ownerID){
-        this._id = id,
-        this._title = title,
-        this._description = description,
-        this._ownerID = ownerID
+        validate(title, description, ownerID)
+        this._id = id;
+        this._title = title;
+        this._description = description;
+        this._ownerID = ownerID;
     }
 
 
 }
 
-module.exports(Category)
+function validate(title, description, ownerID) {
+    const t = typeof title === 'string' ? title.trim() : '';
+    const o = typeof ownerID === 'string' ? title.trim() : '';
+
+    if (!t) {
+      throw new Error('title é obrigatório');
+    }
+    if (!o) {
+      throw new Error('ownerID é obrigatório');
+    }
+}
+
+module.exports = Category
---
+++ src/infra/api/routes/category.routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const router = express.Router();
+
+const Category = require("../../database/schemas/category.schema")
+
+router.post('/create', async (req,res) => {
+    try {
+        const body = req.body
+
+        // criar a categoria
+
+    } catch (error) {
+        
+    }
+})
